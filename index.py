45f978ff28c3995ec5fe94d4bea5ee2b4d966d46
klementomc <85805859+klementomc@users.noreply.github.com>
2021-09-04 21:35:10 +0200

    Update index.py

--- index.py
+++ index.py
@@ -31,6 +31,5 @@ def garaza2():
     time.sleep(2)
     return render_template("stran2.html")
 
-if __name__ == "__main__":
-    app.debug = True
-    app.run()
+if __name__ == '__main__':
+        app.run(debug=True, host='0.0.0.0', port=5000)
