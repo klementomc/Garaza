e09f0d1a13942a423c15b85b2a4641fefa0858d4
klementomc <85805859+klementomc@users.noreply.github.com>
2021-09-05 16:19:24 +0200

    Update index.py

--- index.py
+++ index.py
@@ -19,7 +19,8 @@ def garaza1():
     GPIO.output(7, GPIO.LOW)
     time.sleep(1)
     GPIO.output(7, GPIO.HIGH)
-    time.sleep(2)
+    time.sleep(1)
+    GPIO.output(7, GPIO.LOW)
     return render_template("stran1.html")
 
 
@@ -28,7 +29,8 @@ def garaza2():
     GPIO.output(7, GPIO.LOW)
     time.sleep(1)
     GPIO.output(7, GPIO.HIGH)
-    time.sleep(2)
+    time.sleep(1)
+    GPIO.output(7, GPIO.LOW)
     return render_template("stran2.html")
 
 if __name__ == '__main__':
